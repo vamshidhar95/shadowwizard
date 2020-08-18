c5d7e1f9cba08e8a942b22a30a0c5e051b06fbe4
vamshidhar95 <vamshidhar35@gmail.com>
2020-08-18 17:07:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# what is this?
+
+It is a bundled package
+
+
+# Installation
+
+`npm i shadowwizard --save`
+
+Then...
+
+```
+
+
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+function shadowwizard(params) {
+    let images = document.querySelectorAll('.shadowwizard');
+
+    if(options.shadow_type === "hard"){
+        options.shadow_type = '0px'
+    }else
+        options.shadow_type = '15px'
+
+        images.forEach(image => {
+            image.getElementsByClassName.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.2)`;
+
+            if(options.padding){
+                image.style.padding = '1em'
+            }
+        })
+
+}
+
+modules.exports.shadowwizard = shadowwizard;
